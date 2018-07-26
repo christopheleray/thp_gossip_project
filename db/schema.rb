4424efc98f0cb6d7853c803b196a4186fb143ec0
christopheleray <christophe.leray1@gmail.com>
2018-07-26 21:31:48 +0200

    seed de la db

--- db/schema.rb
+++ db/schema.rb
@@ -46,11 +46,11 @@ ActiveRecord::Schema.define(version: 2018_07_26_174419) do
   end
 
   create_table "likes", force: :cascade do |t|
-    t.string "imageable_type"
-    t.integer "imageable_id"
+    t.string "likeable_type"
+    t.integer "likeable_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["imageable_type", "imageable_id"], name: "index_likes_on_imageable_type_and_imageable_id"
+    t.index ["likeable_type", "likeable_id"], name: "index_likes_on_likeable_type_and_likeable_id"
   end
 
   create_table "private_messages", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,62 @@
 require 'faker'
 
 10.times do
-  city = City.create(name: Faker::Address.city, postal_code: Faker::Address.zip_code)
+  city = City.create(
+    name: Faker::Address.city, 
+    postal_code: Faker::Address.zip_code
+    )
 end
 
 10.times do
-  user = User.create(first_name: Faker::Name.name, last_name: Faker::Name.name , description: Faker::Lorem.paragraph, email: Faker::Internet.email, age: Faker::Number(20, 100))
+  user = User.create(
+    first_name: Faker::Name.name, 
+    last_name: Faker::Name.name,
+    description: Faker::Lorem.paragraph, 
+    email: Faker::Internet.email, 
+    age: Faker::Number.between(20, 100),
+    city_id: rand((City.first.id)..(City.last.id))    
+    )
+end
+
+20.times do
+  gossip = Gossip.create(
+    title: Faker::HowIMetYourMother.character, 
+    content: Faker::HowIMetYourMother.catch_phrase, 
+    #user_id: Faker::Number.between(1, 10)
+    user_id: rand((User.first.id)..(User.last.id)) 
+    )
 end
 
 10.times do
-  gossip = Gossip.create(title: HowIMetYourMother.character, content: Faker::HowIMetYourMother.catch_phrase, user_id: rand((User.first.id)..(User.last.id)))
+  tag = Tag.create(tag: Faker::Music.band)
+end
+
+for i in ((Tag.first.id)..(Tag.last.id))
+  gossip = Gossip.find(i)
+  tag = Tag.find(i)
+  gossip.tags << tag
 end
 
 10.times do
-  comments = Comment.create(content: , gossip_id: , user_id: )
+  gossip_id = rand((Gossip.first.id)..(Gossip.last.id))
+  tag_id = rand((Tag.first.id)..(Tag.last.id))
+  gossip = Gossip.find(gossip_id)
+  tag = Tag.find(tag_id)
+  gossip.tags << tag
 end
 
-10.times do
-  tag = Tag.create()
+1.times do
+  private_message = PrivateMessage.create(
+    content: Faker::Lorem.paragraph,
+    recipient_id: rand((User.first.id)..(User.last.id)),
+    sender_id: rand((User.first.id)..(User.last.id)) 
+  )
 end
 
-10.times do
-  like = 
+20.times do
+  comments = Comment.create(
+    content: Faker::Lorem.paragraph, 
+    gossip_id: rand((Gossip.first.id)..(Gossip.last.id)),   
+    user_id: rand((User.first.id)..(User.last.id)) 
+    )
 end
